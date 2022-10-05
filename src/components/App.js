15eb16976d90b1394e52ea255becfc0f9dde9232
sanjeev singh <sanjeev.rajput1976@gmail.com>
2022-10-05 19:12:11 +0000

    Update package-lock.json and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-import '../styles/App.css';
 
+const searchArray = [
+  "Newton",
+  "School",
+  "Newton School",
+  "React",
+  "Preact",
+  "Node",
+  "Mongoose",
+  "Angular",
+  "Vue",
+  "Apple",
+  "Microsoft",
+  "Netflix",
+  "Meta",
+];
 
-const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
+  const [name, setSearchTerm] = useState("");
 
-  
+
+
   return (
     <div id="main">
+      <input
+        type="text"
+        id="search-input"
+        onChange={(event)=> {
+          setSearchTerm(event.target.value);
+        }}
+        
+      />
+      <p> Result</p>
       
+      <ul> <li> {searchArray.filter((val)=> {
+         if(name == ""){
+          return( name
+      )}
+         else if(val.toLowerCase().includes(name.toLowerCase())){
+            return val
+          }
+})}</li></ul>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
